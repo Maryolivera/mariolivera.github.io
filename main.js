ba60a46ad5e2a634cc26fa0be73802f08e763750
Maryolivera <128436041+Maryolivera@users.noreply.github.com>
2023-10-20 02:16:17 -0300

    formulario de contacto
    validacion del formulario

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-alert("HOLA")
+<script>
+function validar(){
+
+	let nombre= document.getElementById("nombre");
+	let mensaje=document.getElementById("mensaje");
+	let mail=document.getElementById("mail");
+	let ltaErrores=document.getElementById("ltaErrores");
+
+	if (nombre.value==""){
+		console.log("nombre invalido")
+	}
+
+	if (mensaje.value=="" || mensaje.value.length > 200){
+		console.log("mensaje invalido")
+	}
+
+
+	let item=document.createElement("li");
+	ltaErrores.appendChild(item);
+	item.innerHTML="";
+	return false;
+	
+
+}
+</script>
